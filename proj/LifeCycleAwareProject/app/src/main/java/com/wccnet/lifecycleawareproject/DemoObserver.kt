f06a29219219c0eb089b1fc04db71a0903fbb9ab
Autumn Day <alday@wccnet.edu>
2022-11-06 20:53:21 -0500

    nav project completed

--- proj/LifeCycleAwareProject/app/src/main/java/com/wccnet/lifecycleawareproject/DemoObserver.kt
+++ proj/LifeCycleAwareProject/app/src/main/java/com/wccnet/lifecycleawareproject/DemoObserver.kt
@@ -1,6 +1,8 @@
 package com.wccnet.lifecycleawareproject
 
+import android.os.Build
 import android.util.Log
+import androidx.annotation.RequiresApi
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleObserver
 import androidx.lifecycle.OnLifecycleEvent
@@ -12,17 +14,20 @@ class DemoObserver(): LifecycleObserver {
     private val myCoroutineScope = CoroutineScope(Dispatchers.Main)
     var MV = MainViewModel
 
+    @RequiresApi(Build.VERSION_CODES.O)
     @OnLifecycleEvent(Lifecycle.Event.ON_CREATE)
     fun onCreate() {
         MV.addLine("onCreate called")
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
     fun onResume() {
         MV.addLine("onResume called")
         MV.addLine("**************")
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     @OnLifecycleEvent(Lifecycle.Event.ON_START)
     fun onStart() {
         MV.addLine("onStart called")
